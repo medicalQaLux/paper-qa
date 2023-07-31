9883c63da6934b7b10d00ecc23660ca1b76a457f
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2023-07-31 00:04:07 -0400

    deleted small paperqa

--- app.py
+++ app.py
@@ -12,23 +12,16 @@ app = Flask(__name__)
 from paperqa import DocsPineCone  # must come after the .env loads
 
 pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
-pinecone.list_indexes()
-index = pinecone.Index("paperqa-index")
 parquet_file_path = 'docs_index_dets.parquet'
 paperqa_engine = DocsPineCone(text_index_name = "paperqa-index", parquet_file = parquet_file_path)
-small_paperqa_engine = DocsPineCone(text_index_name = "paperqa-openai-check", parquet_file = parquet_file_path)
 
 @app.route('/ask_rag', methods=['POST'])
 def ask_rag():
     req = request.get_json()
     chat_log = req['chat_log']
-    if req['index_size'] == 'small':
-        engine = small_paperqa_engine
-    else:
-        engine = paperqa_engine
     last_question = chat_log[-1]['user'] if chat_log else ''
     
-    a = engine.query(last_question)
+    a = paperqa_engine.query(last_question)
     return a.answer, 200
 
 if __name__ == "__main__":
